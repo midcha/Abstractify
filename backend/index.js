c1aa6ed0c8b4a0f8df731d37fcfc27d55b5dcaff
AvyuktaNagrath <134120674+AvyuktaNagrath@users.noreply.github.com>
2024-10-25 20:28:58 -0400

    Initial Commit
    Set up react app

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((error) => console.log("Error connecting to MongoDB:", error));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
