d470992fbecaf739bb35c0707e859c85816b3d26
Sunoo Jeong <sunoo.jeong@gmail.com>
2024-10-25 23:14:21 -0400

    Implementing Gemini
    Created the Prompts folder and a folder for the pdfs of the research paper.

--- backend/Categorize/Categorize.js
+++ backend/Categorize/Categorize.js
@@ -1,11 +1,30 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleAIFileManager } = require("@google/generative-ai/server")
 require('dotenv').config();
+const fs = require('fs');
+
 
 async function generateContent() {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+    const fileManager = new GoogleAIFileManager(process.env.GEMINI_KEY);
+
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-    const prompt = "Hello";
-    const result = await model.generateContent(prompt);
+
+    const uploadResponse = await fileManager.uploadFile("./TestPapers/Paper1.pdf", {
+        mimeType: "application/pdf",
+        displayName: "ResearchPaperPDF",
+      });
+    
+    const prompt = fs.readFileSync('./Prompts/getJSON.txt', 'utf8');
+    const result = await model.generateContent([
+        {
+          fileData: {
+            mimeType: uploadResponse.file.mimeType,
+            fileUri: uploadResponse.file.uri,
+          },
+        },
+        { text: prompt },
+      ]);
     console.log(result.response.text());
   }
 generateContent();
