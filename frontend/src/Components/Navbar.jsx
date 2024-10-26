8c58465876b1aa9b64dbac5d7521a35bdc71fd19
Michael Chan <75458728+midcha@users.noreply.github.com>
2024-10-26 13:15:53 -0400

    Home page and navbar implementation and styling

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,15 +1,33 @@
 // src/Navbar.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom'; 
+import styles from '../CSS files/Navbar.module.css';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/contact">Contact</Link></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [hasOpened, setHasOpened] = useState(false);
+  const toggleMenu = () => {
+    setMenuVisible(prevState => !prevState);
+    setHasOpened(true);
+  };
+
+  return(
+    <div className={styles.navbar}>
+      <button id={styles.dropdown} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+      {menuVisible && <div className={styles.overlay} onClick={toggleMenu} />}
+      <div className={`${styles.links} ${menuVisible ? styles.show : (hasOpened ? styles.hide : styles.hidden)}`}>
+        <a href="/Upload">Upload</a>
+        <a href="/Login">Login</a>
+        <a href="About">About</a>
+      </div>
+    </div>
+    
+  );
+};
 
 export default Navbar;
