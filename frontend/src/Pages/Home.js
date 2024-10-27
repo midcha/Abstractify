924728ab8d8faaf787a8c8f12d3122c3d55ff4cc
Michael Chan <75458728+midcha@users.noreply.github.com>
2024-10-27 02:58:04 -0400

    Put in template images and text for the home page

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../CSS files/Home.css';
 import geko from '../Images/geko.jpg';
+import sample from '../Images/sample_abstract.jpg';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
@@ -26,12 +27,36 @@ const Home = () => {
           </div>
           <img src={geko} alt="girl" />  
         </div>
-        <div id='infoBox' className="demos">
-
+        <div id='imgContentBar'>
+          <div id="imgCaption">
+            <img src={sample} alt="sample output" />
+            <h4>
+              Sample output from ____
+            </h4>
+          </div>
+          <div id='infoBox' className="demos">
+            <h2>
+              How to Use
+            </h2>
+            <ol type="1">
+              <li>
+                Navigate to the <span className="blueBold">Upload PDF</span> tab up top or click on the 
+                <span className="blueBold"> Get Started</span> button in the bottom right
+              </li>
+              <li>
+                Upload the pdf file of the research paper you desire to generate an abstraction of
+              </li>
+              <li>
+                Wait for the output to generate and viola! You're done!
+              </li>
+            </ol>
+          </div>
         </div>
         <div className="bottom">
           <div id='infoBox' className="contact">
-
+            <h2>
+              Extra Information
+            </h2>
           </div>
           <div className="getStarted">
             <Link to="/Upload">Get Started</Link>
